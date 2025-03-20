de8bc87c5ba446f8d2ea8d7f315473a83c3766d1
Jack English <j.english@spherical-systems.com>
2025-03-20 13:00:11 +0100

    add jedi-language-sever back in

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,94 +1,89 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		-- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"jsonls",
-					"rust_analyzer",
-					"gopls",
-					-- "jedi_language_server",
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "jsonls",
+          "rust_analyzer",
+          "gopls",
+          "jedi_language_server",
           "ruff",
-					"ts_ls",
-					"html",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+          "ts_ls",
+          "html",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- lua
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-
-			-- javascript / typescript
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-
-			-- rust
-			lspconfig.rust_analyzer.setup({
-				settings = {
-					["rust-analyzer"] = {},
-				},
-				capabilities = capabilities,
-			})
-
-			-- python
-			-- lspconfig.jedi_language_server.setup({
-			-- 	capabilities = capabilities,
-			-- })
-
-			-- lspconfig.ruff.setup({
-			--   -- init_options = {
-			--   --      settings = {
-			--   --        logLevel = 'debug',
-			--   --      }
-			--   --    },
-			--   --    capabilities = capabilities,
-			--    })
-      require('lspconfig').ruff.setup({
-        init_options = {
-          settings = {
-            -- Ruff language server settings go here
-          }
-        }
+      -- lua
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
       })
 
-			-- golang
-			lspconfig.gopls.setup({
-				settings = {
-					gopls = {
-						analyses = {
-							unusedparams = true,
-						},
-						staticcheck = true,
-						gofumpt = true,
-					},
-				},
-				capabilities = capabilities,
-			})
-		end,
-	},
+      -- javascript / typescript
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+
+      -- rust
+      lspconfig.rust_analyzer.setup({
+        settings = {
+          ["rust-analyzer"] = {},
+        },
+        capabilities = capabilities,
+      })
+
+      -- python
+      lspconfig.jedi_language_server.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.ruff.setup({
+        capabilities = capabilities,
+      })
+      -- require('lspconfig').ruff.setup({
+      --   init_options = {
+      --     settings = {
+      --       -- Ruff language server settings go here
+      --     }
+      --   }
+      -- })
+
+      -- golang
+      lspconfig.gopls.setup({
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+          },
+        },
+        capabilities = capabilities,
+      })
+    end,
+  },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,9 +2,11 @@ return {
   "nvimtools/none-ls.nvim",
   dependencies = {
     "nvimtools/none-ls-extras.nvim",
+    "jay-babu/mason-null-ls.nvim",
   },
   config = function()
     local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
@@ -13,13 +15,26 @@ return {
         null_ls.builtins.formatting.goimports,
 
         -- python
-        null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.formatting.ruff,
+        -- null_ls.builtins.formatting.ruff,
+        require('none-ls.formatting.ruff').with { extra_args = {'--extend-select', 'I'} },
+        require('none-ls.formatting.ruff_format'),
 
         -- javascript, typescript, HTML
         null_ls.builtins.formatting.prettier,
-        -- null_ls.builtins.diagnostics.biome,
       },
+      -- configure format on save
+      on_attach = function(current_client, bufnr)
+        if current_client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
   end,
