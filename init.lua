5f66984f8be79169b56e599ab80d5d9f97565f72
Jack English <j.english@spherical-systems.com>
2025-04-19 11:00:22 +0200

    add some yaml file associations

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 require("vim-opts")
 require("config.lazy")
+
+vim.filetype.add({
+  extension = {
+    op = "yaml",
+    dc = "yaml",
+    ac = "yaml",
+    noise = "yaml",
+    tran = "yaml",
+    config = "yaml",
+  },
+})
