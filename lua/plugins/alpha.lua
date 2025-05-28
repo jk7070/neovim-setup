4685b0ec7213eb570c380eaa576ab80a3a89e4cb
Jack English <j.english@spherical-systems.com>
2025-05-28 08:35:40 +0200

    alpha update

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,7 +1,22 @@
-return { 
-  'goolord/alpha-nvim',
-  dependencies = { 'echasnovski/mini.icons' },
-  config = function ()
-    require'alpha'.setup(require'alpha.themes.startify'.config)
+-- return {
+--   'goolord/alpha-nvim',
+--   dependencies = { 'echasnovski/mini.icons' },
+--   config = function ()
+--     require'alpha'.setup(require'alpha.themes.startify'.config)
+--   end,
+-- }
+
+return {
+  "goolord/alpha-nvim",
+  -- dependencies = { 'echasnovski/mini.icons' },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local startify = require("alpha.themes.startify")
+    -- available: devicons, mini, default is mini
+    -- if provider not loaded and enabled is true, it will try to use another provider
+    startify.file_icons.provider = "devicons"
+    require("alpha").setup(
+      startify.config
+    )
   end,
 }
